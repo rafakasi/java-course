d0750ccb6d4f349c63c3e07f9a9a118496d36496
rafakasi <rafaelrksilva@gmail.com>
2022-07-31 18:36:50 -0300

    lessons

--- day-01/src/LessonThree.java
+++ day-01/src/LessonThree.java
@@ -1,3 +1,21 @@
 public class LessonThree {
+    public static void main (String[] args) {
+        sum(10, 15);
+        sum(30, 30);
+        sum(20, 20);
+        sum(11, 80);
+        sum(50, 10);
+        sum(80, 5);
+    }
+
+    static void sum(int x, int y) {
+        // static void > default statement
+        // sum > name function
+        // () > define what arrive | type and name
+        // {} > code block to run
+
+        System.out.println(x + y);
+        System.out.println(x - y);
+    }
 
 }
