b1be7a9034bb7a1a83d59d998a38965ecaa439a5
rafakasi <rafaelrksilva@gmail.com>
2022-07-31 19:10:53 -0300

    lessons

---
+++ day-01/src/UnderstandArgs.java
@@ -0,0 +1,8 @@
+public class UnderstandArgs {
+    public static void main(String[] args) {
+        System.out.println("A compiled hello for you " + args[0]);
+        System.out.println("A compiled hello for you " + args[1]);
+        System.out.println("A compiled hello for you " + args[2]);
+        System.out.println("A compiled hello for you " + args[3]);
+    }
+}
