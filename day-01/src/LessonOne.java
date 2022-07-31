0032cafaca044c1f147a90de61caed2abbf98aa2
rafakasi <rafaelrksilva@gmail.com>
2022-07-31 18:22:56 -0300

    lessons

---
+++ day-01/src/LessonOne.java
@@ -0,0 +1,10 @@
+public class LessonOne {
+    public static void main(String[] args) {
+        myInstructionPrint();
+    }
+
+    static void myInstructionPrint() {
+        System.out.println("Hello World");
+    }
+
+}
---
+++ day-01/src/LessonTwo.java
@@ -0,0 +1,18 @@
+public class LessonTwo {
+    public static void main(String[] args) {
+        // var -> types: text (string)
+        // var -> types: number (int)
+        // var -> types: fractional numbers (double)
+        // var -> types: booleans (true | false)
+
+        String login = "rafaelrk";
+        int password = 123456;
+        double percent = 24.30;
+        boolean isDriver = true;
+
+        System.out.println(login);
+        System.out.println(password);
+        System.out.println(percent);
+        System.out.println(isDriver);
+    }
+}
